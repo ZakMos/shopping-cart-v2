d9341a6434fcff47b5b6bd58421a935319875c1d
ZakMos <z.zza@hotmail.com>
2020-03-10 07:40:41 +0100

    add last update to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ class Header extends Component {
       <header className="main-header">
         <img src={logo} className="header-logo" alt="logo" />
         <h1 className="header-title">Welcome to Shopping</h1>
+        <p>last update 10.03.2020</p>
       </header>
     )
   }
