f7cf2cd090cbae0b3a53a574fb1340cfaa118385
ZakMos <z.zza@hotmail.com>
2020-03-10 07:28:14 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,21 @@ class Footer extends Component {
   render() {
   return (
     <footer className="main-footer">
-      <h3 className="band-name"> Footer</h3>
+      <h3 className="band-name">&copy; 2020 Zakaria Moslim </h3>
+        <div className="contact">
+          <a href="https://github.com/ZakMos" target="_blank" rel="noopener noreferrer">
+            <img src="http://icons.iconarchive.com/icons/papirus-team/papirus-apps/24/github-icon.png" alt="github"/>
+          </a>
+          <a href="https://zakmos.github.io" target="_blank" rel="noopener noreferrer">
+            <img src="http://icons.iconarchive.com/icons/social-media-icons/glossy-social/24/Website-icon.png" alt="website"/>
+            </a>
+            <a href="https://de.linkedin.com/in/zakmos" target="_blank" rel="noopener noreferrer">
+            <img src="http://icons.iconarchive.com/icons/martz90/circle/24/linkedin-icon.png" alt="linkedin"/>
+            </a>
+            <a href="mailto:z.zza@hotmail.com" target="_blank" rel="noopener noreferrer">
+            <img src="http://icons.iconarchive.com/icons/graphicloads/100-flat/24/email-2-icon.png" alt="EMail" />
+            </a>
+        </div>
     </footer>
     )
   }
